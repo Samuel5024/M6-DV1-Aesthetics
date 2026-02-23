0cdbf6d2fe879742c9e5f8a69e520b07128414d3
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-02-23 02:34:47 -0700

    create a fog toggle

--- M6_DV1_Aesthetics/Assets/Scripts/EnvironmentLightingControls.cs
+++ M6_DV1_Aesthetics/Assets/Scripts/EnvironmentLightingControls.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
+using TMPro;
 
 public class EnvironmentLightingControls : MonoBehaviour
 {
     public Toggle useSkyboxToggle;
+    public Toggle enableFog;
+    public Slider skyboxIntensitySlider;
+    //public Text skyboxIntensityText;
+    public TextMeshProUGUI skyboxIntensityText;
 
     public bool UseSkybox
     {
@@ -13,12 +18,32 @@ public class EnvironmentLightingControls : MonoBehaviour
         {
             RenderSettings.ambientMode = (value) ?
                 AmbientMode.Skybox : AmbientMode.Flat;
+            skyboxIntensitySlider.interactable = value;
+            skyboxIntensityText.gameObject.SetActive(value);
+        }
+    }
+
+    public bool EnableFog
+    {
+        get => RenderSettings.fog;
+        set => RenderSettings.fog = value;
+    }
+
+    public float SkyboxIntensity
+    {
+        get => RenderSettings.ambientIntensity;
+        set
+        {
+            RenderSettings.ambientIntensity = value;
+            skyboxIntensityText.text = value.ToString("F1");
         }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         useSkyboxToggle.isOn = UseSkybox;
+        skyboxIntensitySlider.value = SkyboxIntensity;
+        enableFog.isOn = EnableFog;
     }
 
     // Update is called once per frame
